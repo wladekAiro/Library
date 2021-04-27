cb42b972db8d4c71841b8c6f33f60c2e9797b140
Wladek Airo <wladek.airo@gmail.com>
2021-04-27 18:43:10 +0300

    project set up

---
+++ src/test/java/com/wladek/lib/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wladek.lib.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
